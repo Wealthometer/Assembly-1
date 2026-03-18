c02341caba891ff1d4a21b82feac76bb5bf81eee
Wealthometer <wealthhajoh87@gmail.com>
2026-03-19 00:21:42 +0100

    Update main.s

--- main.s
+++ main.s
@@ -3,6 +3,19 @@
 .intel_syntax noprefix
 
  _start:
+
+        // sys_write
+        mov rax, 1
+        mov rdi, 1
+        lea rsi, [hello_world]
+        mov rdx, 17
+        syscall
+        
+
+        // sys_exit
          mov rax, 60
          mov rdi, 69
          syscall
+
+hello_world:
+        .asciz "Hello, World..!!\n"
